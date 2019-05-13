cbbb9ff2a9c96c24980676f8f84960f58ae6191d
Kioshi <kioshimneal@msn.com>
2019-05-13 12:48:28 -0400

    Push Lab 11 to GitHub

---
+++ Lab11/Movie.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab11
+{
+    class Movie
+    {
+        private string Title { get; set; }
+        private string Category { get; set; }
+
+        public Movie(string title, string category)
+        {
+            Title = title;
+            Category = category;
+        }
+
+        public static void GetHorror(List<Movie> movieList)
+        {
+            foreach (var movie in movieList)
+            {
+                if (movie.Category.Equals("Horror", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine(movie.Title);
+                }
+            }
+        }
+
+        public static void GetSciFi(List<Movie> movieList)
+        {
+            foreach (var movie in movieList)
+            {
+                if (movie.Category.Equals("SciFi", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine(movie.Title);
+                }
+            }
+        }
+
+        public static void GetAnimated(List<Movie> movieList)
+        {
+            foreach (var movie in movieList)
+            {
+                if (movie.Category.Equals("Animated", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine(movie.Title);
+                }
+            }
+        }
+
+        public static void GetDrama(List<Movie> movieList)
+        {
+            foreach (var movie in movieList)
+            {
+                if (movie.Category.Equals("Drama", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine(movie.Title);
+                }
+            }
+        }
+    }
+
+}
---
+++ Lab11/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Net.WebRequestMethods;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Movie> movies = new List<Movie>();
+
+            movies.Add(new Movie("Coco", "Animated"));
+            movies.Add(new Movie("Southpaw", "Drama"));
+            movies.Add(new Movie("Poltergeist", "Horror"));
+            movies.Add(new Movie("Arrival", "SciFi"));
+            movies.Add(new Movie("Zootopia", "Animated"));
+            movies.Add(new Movie("Loft", "Drama"));
+            movies.Add(new Movie("Carrie", "Horror"));
+            movies.Add(new Movie("Avatar", "SciFi"));
+            movies.Add(new Movie("Inception", "SciFi"));
+            movies.Add(new Movie("Alladin", "Animated"));
+
+
+            Console.WriteLine("Welcome to the Movie List Application");
+
+            char continueLoop;
+
+               do
+               {
+                    Console.WriteLine("What movie type are you interested in?");
+                    Console.Write("You're options are Animated, Drama, SciFi and Horror ");
+                    var userInput = Console.ReadLine();
+
+                    if (userInput.Equals("Horror", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Movie.GetHorror(movies);
+                    }
+                    else if (userInput.Equals("Animated", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Movie.GetAnimated(movies);
+                    }
+                    else if (userInput.Equals("SciFi", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Movie.GetSciFi(movies);
+                    }
+                    else if (userInput.Equals("Drama", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Movie.GetDrama(movies);
+                    }
+                    else
+                    {
+                        Console.WriteLine("That is an invalid choice.");
+                    }
+
+                    Console.Write("Would you like to continue (y/n)? ");
+                    continueLoop = char.Parse(Console.ReadLine().ToUpper());
+                        
+
+
+               } while (continueLoop.Equals(char.Parse("Y")));
+            
+            
+            Console.WriteLine("Have a Great Day!!");
+            Console.ReadKey();
+            
+        }
+    }
+}
